ee70d2df3c99cbf511c8c343678c6bf81a0a25c5
Fabio Moura <fabio.siq.moura@gmail.com>
2022-02-10 16:38:09 -0300

    feat: modified sign up button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, Box, Container, Button } from 'theme-ui';
-import Sticky from 'react-stickynode';
-import Logo from 'components/logo';
-import { NavLink } from 'components/link';
-import { DrawerProvider } from 'contexts/drawer/drawer-provider';
-import NavbarDrawer from './navbar-drawer';
-import menuItems from './header.data';
+import { jsx, Box, Container, Button } from "theme-ui";
+import Sticky from "react-stickynode";
+import Logo from "components/logo";
+import { NavLink } from "components/link";
+import { DrawerProvider } from "contexts/drawer/drawer-provider";
+import NavbarDrawer from "./navbar-drawer";
+import menuItems from "./header.data";
 
 export default function Header() {
   return (
@@ -26,11 +26,11 @@ export default function Header() {
                     ))}
                   </Box>
                   <Button variant="text" sx={styles.getStartedDesktop}>
-                    Get Started
+                    Sign Up Desktop
                   </Button>
                 </Box>
                 <Button variant="text" sx={styles.getStartedMobile}>
-                  Get Started
+                  Sign Up Mobile
                 </Button>
                 <NavbarDrawer />
               </Box>
@@ -44,79 +44,79 @@ export default function Header() {
 
 const styles = {
   headerWrapper: {
-    backgroundColor: 'transparent',
-    '.is-sticky': {
+    backgroundColor: "transparent",
+    ".is-sticky": {
       header: {
-        backgroundColor: 'white',
-        boxShadow: '0 6px 13px rgba(38,78,118,0.1)',
-        paddingTop: '15px',
-        paddingBottom: '15px',
+        backgroundColor: "white",
+        boxShadow: "0 6px 13px rgba(38,78,118,0.1)",
+        paddingTop: "15px",
+        paddingBottom: "15px",
       },
     },
   },
   header: {
-    position: 'fixed',
+    position: "fixed",
     left: 0,
     right: 0,
     py: 4,
-    transition: 'all 0.3s ease-in-out 0s',
-    '&.is-mobile-menu': {
-      backgroundColor: 'white',
+    transition: "all 0.3s ease-in-out 0s",
+    "&.is-mobile-menu": {
+      backgroundColor: "white",
     },
   },
   headerInner: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   logo: {
     mr: [null, null, null, null, 6, 12],
   },
   navbar: {
-    display: ['none', null, null, null, 'flex'],
-    alignItems: 'center',
+    display: ["none", null, null, null, "flex"],
+    alignItems: "center",
     flexGrow: 1,
     // justifyContent: 'center',
   },
   navList: {
-    display: ['flex'],
-    listStyle: 'none',
+    display: ["flex"],
+    listStyle: "none",
     // marginLeft: 'auto',
     flexGrow: 1,
     p: 0,
-    'li:last-child': {
-      ml: ['auto'],
+    "li:last-child": {
+      ml: ["auto"],
     },
-    '.nav-item': {
-      cursor: 'pointer',
+    ".nav-item": {
+      cursor: "pointer",
       fontWeight: 400,
       padding: 0,
-      margin: [0, 0, 0, 0, '0 20px'],
+      margin: [0, 0, 0, 0, "0 20px"],
     },
-    '.active': {
-      color: 'primary',
+    ".active": {
+      color: "primary",
     },
   },
   getStartedDesktop: {
-    color: 'primary',
-    display: ['none', 'none', 'none', 'none', 'flex'],
+    color: "primary",
+    display: ["none", "none", "none", "none", "flex"],
   },
   getStartedMobile: {
-    color: 'primary',
+    color: "primary",
     fontSize: [1],
     minHeight: 30,
-    m: ['0 15px 0 auto'],
-    padding: '0 11px',
-    display: ['flex', null, null, null, 'none'],
+    m: ["0 15px 0 auto"],
+    padding: "0 11px",
+    display: ["flex", null, null, null, "none"],
   },
   closeButton: {
-    height: '32px',
-    padding: '4px',
-    minHeight: 'auto',
-    width: '32px',
-    ml: '3px',
+    height: "32px",
+    padding: "4px",
+    minHeight: "auto",
+    width: "32px",
+    ml: "3px",
     path: {
-      stroke: 'text',
+      stroke: "text",
     },
   },
 };
